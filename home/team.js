0ee190499c821b73d5aa3642607993bbe40d470c
Arnaud <arnaud@status.im>
2025-07-02 20:35:11 +0200

    Add embla for team

--- home/team.js
+++ home/team.js
@@ -1,59 +1,60 @@
-let index = 0;
-let currentContributor = document.getElementById("contributor-0");
-let isMouseHover = false;
-let footer = document.querySelector(".team footer");
+// let index = 0;
+// let currentContributor = document.getElementById("contributor-0");
+// let isMouseHover = false;
+// let footer = document.querySelector(".team footer");
 
-footer.addEventListener("mouseover", () => {
-  isMouseHover = true;
-});
+// footer.addEventListener("mouseover", () => {
+//   isMouseHover = true;
+// });
 
-footer.addEventListener("mouseout", () => {
-  console.info("Mouse out of footer");
-  // Reset the index to 0 when mouse leaves the footer
-  isMouseHover = false;
-});
+// footer.addEventListener("mouseout", () => {
+//   console.info("Mouse out of footer");
+//   // Reset the index to 0 when mouse leaves the footer
+//   isMouseHover = false;
+// });
 
-currentContributor.classList.add("default");
+// currentContributor.classList.add("default");
 
-function changeCoreContributor() {
-  if (isMouseHover) {
-    return false;
-  }
+// function changeCoreContributor() {
+//   if (isMouseHover) {
+//     return false;
+//   }
 
-  currentContributor.classList.remove("default");
+//   currentContributor.classList.remove("default");
 
-  let nextContributor = document.getElementById("contributor-" + (index + 1));
-  if (!nextContributor) {
-    nextContributor = document.getElementById("contributor-0");
-    index = 0;
-  }
+//   let nextContributor = document.getElementById("contributor-" + (index + 1));
+//   if (!nextContributor) {
+//     nextContributor = document.getElementById("contributor-0");
+//     index = 0;
+//   }
 
-  nextContributor.classList.add("default");
+//   nextContributor.classList.add("default");
 
-  const nextContributorSrc = nextContributor
-    .querySelector("img")
-    .getAttribute("src");
+//   const nextContributorSrc = nextContributor
+//     .querySelector("img")
+//     .getAttribute("src");
 
-  // Get the parent container
-  const parentContainer = nextContributor.parentElement;
+//   // Get the parent container
+//   const parentContainer = nextContributor.parentElement;
 
-  // Calculate the horizontal scroll position
-  const offsetLeft = nextContributor.offsetLeft;
-  const containerWidth = parentContainer.offsetWidth;
-  const contributorWidth = nextContributor.offsetWidth;
+//   // Calculate the horizontal scroll position
+//   const offsetLeft = nextContributor.offsetLeft;
+//   const containerWidth = parentContainer.offsetWidth;
+//   const contributorWidth = nextContributor.offsetWidth;
 
-  parentContainer.scrollTo({
-    left: offsetLeft - containerWidth / 2 + contributorWidth / 2,
-    behavior: "smooth", // Enable smooth scrolling
-  });
+//   parentContainer.scrollTo({
+//     left: offsetLeft - containerWidth / 2 + contributorWidth / 2,
+//     behavior: "smooth", // Enable smooth scrolling
+//   });
 
-  const mainContributorImage = document.getElementById(
-    "main-contributor-image"
-  );
-  mainContributorImage.setAttribute("src", nextContributorSrc);
+//   const mainContributorImage = document.getElementById(
+//     "main-contributor-image"
+//   );
+//   mainContributorImage.setAttribute("src", nextContributorSrc);
 
-  index++;
-  currentContributor = nextContributor;
-}
+//   index++;
+//   currentContributor = nextContributor;
+// }
 
-setInterval(changeCoreContributor, 5000);
+// setInterval(changeCoreContributor, 5000);
+
--- home/usecases.js
+++ home/usecases.js
@@ -37,183 +37,281 @@ usecases.forEach((usecase) => {
   });
 });
 
-if (window.innerWidth <= 1000) {
-  function createScriptElement(src, onloadCallback) {
-    const script = document.createElement("script");
-    script.src = src;
-    script.onload = onloadCallback;
-    document.head.appendChild(script);
-  }
+function createScriptElement(src, onloadCallback) {
+  const script = document.createElement("script");
+  script.src = src;
+  script.onload = onloadCallback;
+  document.head.appendChild(script);
+}
 
-  const addTogglePrevNextBtnsActive = (emblaApi, prevBtn, nextBtn) => {
-    const togglePrevNextBtnsState = () => {
-      if (emblaApi.canScrollPrev()) prevBtn.removeAttribute("disabled");
-      else prevBtn.setAttribute("disabled", "disabled");
+const addTogglePrevNextBtnsActive = (emblaApi, prevBtn, nextBtn) => {
+  const togglePrevNextBtnsState = () => {
+    if (emblaApi.canScrollPrev()) prevBtn.removeAttribute("disabled");
+    else prevBtn.setAttribute("disabled", "disabled");
 
-      if (emblaApi.canScrollNext()) nextBtn.removeAttribute("disabled");
-      else nextBtn.setAttribute("disabled", "disabled");
-    };
-
-    emblaApi
-      .on("select", togglePrevNextBtnsState)
-      .on("init", togglePrevNextBtnsState)
-      .on("reInit", togglePrevNextBtnsState);
-
-    return () => {
-      prevBtn.removeAttribute("disabled");
-      nextBtn.removeAttribute("disabled");
-    };
+    if (emblaApi.canScrollNext()) nextBtn.removeAttribute("disabled");
+    else nextBtn.setAttribute("disabled", "disabled");
   };
 
-  const addPrevNextBtnsClickHandlers = (emblaApi, prevBtn, nextBtn) => {
-    const scrollPrev = () => {
-      emblaApi.scrollPrev();
-    };
-    const scrollNext = () => {
-      emblaApi.scrollNext();
-    };
-    prevBtn.addEventListener("click", scrollPrev, false);
-    nextBtn.addEventListener("click", scrollNext, false);
+  emblaApi
+    .on("select", togglePrevNextBtnsState)
+    .on("init", togglePrevNextBtnsState)
+    .on("reInit", togglePrevNextBtnsState);
 
-    const removeTogglePrevNextBtnsActive = addTogglePrevNextBtnsActive(
-      emblaApi,
-      prevBtn,
-      nextBtn
-    );
-
-    return () => {
-      removeTogglePrevNextBtnsActive();
-      prevBtn.removeEventListener("click", scrollPrev, false);
-      nextBtn.removeEventListener("click", scrollNext, false);
-    };
+  return () => {
+    prevBtn.removeAttribute("disabled");
+    nextBtn.removeAttribute("disabled");
   };
+};
 
-  const addDotBtnsAndClickHandlers = (emblaApi, dotsNode) => {
-    let dotNodes = [];
-
-    const addDotBtnsWithClickHandlers = () => {
-      dotsNode.innerHTML = emblaApi
-        .scrollSnapList()
-        .map(() => '<button class="dot" type="button"></button>')
-        .join("");
-
-      const scrollTo = (index) => {
-        emblaApi.scrollTo(index);
-      };
-
-      dotNodes = Array.from(dotsNode.querySelectorAll(".dot"));
-      dotNodes.forEach((dotNode, index) => {
-        dotNode.addEventListener("click", () => scrollTo(index), false);
-      });
-    };
-
-    const toggleDotBtnsActive = () => {
-      const previous = emblaApi.previousScrollSnap();
-      const selected = emblaApi.selectedScrollSnap();
-      dotNodes[previous].removeAttribute("aria-selected");
-      dotNodes[selected].setAttribute("aria-selected", "true");
-    };
-
-    emblaApi
-      .on("init", addDotBtnsWithClickHandlers)
-      .on("reInit", addDotBtnsWithClickHandlers)
-      .on("init", toggleDotBtnsActive)
-      .on("reInit", toggleDotBtnsActive)
-      .on("select", toggleDotBtnsActive);
-
-    return () => {
-      dotsNode.innerHTML = "";
-    };
+const addPrevNextBtnsClickHandlers = (emblaApi, prevBtn, nextBtn) => {
+  const scrollPrev = () => {
+    emblaApi.scrollPrev();
   };
+  const scrollNext = () => {
+    emblaApi.scrollNext();
+  };
+  prevBtn.addEventListener("click", scrollPrev, false);
+  nextBtn.addEventListener("click", scrollNext, false);
 
-  const TWEEN_FACTOR_BASE = 0.52;
-  let tweenFactor = 0;
-  let tweenNodes = [];
+  const removeTogglePrevNextBtnsActive = addTogglePrevNextBtnsActive(
+    emblaApi,
+    prevBtn,
+    nextBtn
+  );
 
-  const numberWithinRange = (number, min, max) =>
-    Math.min(Math.max(number, min), max);
+  return () => {
+    removeTogglePrevNextBtnsActive();
+    prevBtn.removeEventListener("click", scrollPrev, false);
+    nextBtn.removeEventListener("click", scrollNext, false);
+  };
+};
 
-  const setTweenNodes = (emblaApi) => {
-    tweenNodes = emblaApi.slideNodes().map((slideNode) => {
-      return slideNode;
+const addDotBtnsAndClickHandlers = (emblaApi, dotsNode) => {
+  let dotNodes = [];
+
+  const addDotBtnsWithClickHandlers = () => {
+    dotsNode.innerHTML = emblaApi
+      .scrollSnapList()
+      .map(() => '<button class="dot" type="button"></button>')
+      .join("");
+
+    const scrollTo = (index) => {
+      emblaApi.scrollTo(index);
+    };
+
+    dotNodes = Array.from(dotsNode.querySelectorAll(".dot"));
+    dotNodes.forEach((dotNode, index) => {
+      dotNode.addEventListener("click", () => scrollTo(index), false);
     });
   };
 
-  const setTweenFactor = (emblaApi) => {
-    tweenFactor = TWEEN_FACTOR_BASE * emblaApi.scrollSnapList().length;
+  const toggleDotBtnsActive = () => {
+    const previous = emblaApi.previousScrollSnap();
+    const selected = emblaApi.selectedScrollSnap();
+    dotNodes[previous].removeAttribute("aria-selected");
+    dotNodes[selected].setAttribute("aria-selected", "true");
   };
 
-  const tweenScale = (emblaApi, eventName) => {
-    const engine = emblaApi.internalEngine();
-    const scrollProgress = emblaApi.scrollProgress();
-    const slidesInView = emblaApi.slidesInView();
-    const isScrollEvent = eventName === "scroll";
+  emblaApi
+    .on("init", addDotBtnsWithClickHandlers)
+    .on("reInit", addDotBtnsWithClickHandlers)
+    .on("init", toggleDotBtnsActive)
+    .on("reInit", toggleDotBtnsActive)
+    .on("select", toggleDotBtnsActive);
 
-    emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
-      let diffToTarget = scrollSnap - scrollProgress;
-      const slidesInSnap = engine.slideRegistry[snapIndex];
+  return () => {
+    dotsNode.innerHTML = "";
+  };
+};
 
-      slidesInSnap.forEach((slideIndex) => {
-        if (isScrollEvent && !slidesInView.includes(slideIndex)) return;
+const TWEEN_FACTOR_BASE = 0.52;
+let tweenFactor = 0;
+let tweenNodes = [];
 
-        if (engine.options.loop) {
-          engine.slideLooper.loopPoints.forEach((loopItem) => {
-            const target = loopItem.target();
+const numberWithinRange = (number, min, max) =>
+  Math.min(Math.max(number, min), max);
 
-            if (slideIndex === loopItem.index && target !== 0) {
-              const sign = Math.sign(target);
+const setTweenNodes = (emblaApi) => {
+  tweenNodes = emblaApi.slideNodes().map((slideNode) => {
+    return slideNode;
+  });
+};
 
-              if (sign === -1) {
-                diffToTarget = scrollSnap - (1 + scrollProgress);
-              }
-              if (sign === 1) {
-                diffToTarget = scrollSnap + (1 - scrollProgress);
-              }
+const setTweenFactor = (emblaApi) => {
+  tweenFactor = TWEEN_FACTOR_BASE * emblaApi.scrollSnapList().length;
+};
+
+const tweenScale = (emblaApi, eventName) => {
+  const engine = emblaApi.internalEngine();
+  const scrollProgress = emblaApi.scrollProgress();
+  const slidesInView = emblaApi.slidesInView();
+  const isScrollEvent = eventName === "scroll";
+
+  emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
+    let diffToTarget = scrollSnap - scrollProgress;
+    const slidesInSnap = engine.slideRegistry[snapIndex];
+
+    slidesInSnap.forEach((slideIndex) => {
+      if (isScrollEvent && !slidesInView.includes(slideIndex)) return;
+
+      if (engine.options.loop) {
+        engine.slideLooper.loopPoints.forEach((loopItem) => {
+          const target = loopItem.target();
+
+          if (slideIndex === loopItem.index && target !== 0) {
+            const sign = Math.sign(target);
+
+            if (sign === -1) {
+              diffToTarget = scrollSnap - (1 + scrollProgress);
             }
-          });
-        }
+            if (sign === 1) {
+              diffToTarget = scrollSnap + (1 - scrollProgress);
+            }
+          }
+        });
+      }
 
-        const tweenValue = 1 - Math.abs(diffToTarget * tweenFactor);
-        const scale = numberWithinRange(tweenValue, 0, 1).toString();
-        const tweenNode = tweenNodes[slideIndex];
-        const opacity = numberWithinRange(tweenValue, 0, 1).toString();
-        tweenNode.style.transform = `scale(${scale})`;
-        tweenNode.style.opacity = opacity;
-      });
+      const tweenValue = 1 - Math.abs(diffToTarget * tweenFactor);
+      const scale = numberWithinRange(tweenValue, 0, 1).toString();
+      const tweenNode = tweenNodes[slideIndex];
+      const opacity = numberWithinRange(tweenValue, 0, 1).toString();
+      tweenNode.style.transform = `scale(${scale})`;
+      tweenNode.style.opacity = opacity;
     });
+  });
+};
+
+const setupTweenScale = (emblaApi) => {
+  setTweenNodes(emblaApi);
+  setTweenFactor(emblaApi);
+  tweenScale(emblaApi);
+
+  emblaApi
+    .on("reInit", setTweenNodes)
+    .on("reInit", setTweenFactor)
+    .on("reInit", tweenScale)
+    .on("scroll", tweenScale)
+    .on("slideFocus", tweenScale);
+
+  return () => {
+    tweenNodes.forEach((slide) => slide.removeAttribute("style"));
+  };
+};
+
+const addAutoplayProgressListeners = (emblaApi, progressNode) => {
+  const progressBarNode = document.querySelector(".embla-progress-bar");
+
+  let animationName = "";
+  let timeoutId = 0;
+  let rafId = 0;
+  console.info("addAutoplayProgressListeners", progressNode);
+
+  const startProgress = (emblaApi) => {
+    console.info("startProgress");
+
+    const autoplay = emblaApi?.plugins()?.autoplay;
+    if (!autoplay) return;
+    console.info("okk");
+    const timeUntilNext = autoplay.timeUntilNext();
+    if (timeUntilNext === null) return;
+
+    if (!animationName) {
+      const style = window.getComputedStyle(progressBarNode);
+      animationName = style.animationName;
+    }
+
+    progressBarNode.style.animationName = "none";
+    progressBarNode.style.transform = "translate3d(0,0,0)";
+
+    rafId = window.requestAnimationFrame(() => {
+      timeoutId = window.setTimeout(() => {
+        progressBarNode.style.animationName = animationName;
+        progressBarNode.style.animationDuration = `${timeUntilNext}ms`;
+      }, 0);
+    });
+
+    progressNode.removeAttribute("aria-hidden");
   };
 
-  const setupTweenScale = (emblaApi) => {
-    setTweenNodes(emblaApi);
-    setTweenFactor(emblaApi);
-    tweenScale(emblaApi);
+  const stopProgress = (emblaApi) => {
+    const autoplay = emblaApi?.plugins()?.autoplay;
+    if (!autoplay) return;
+
+    progressNode.setAttribute("aria-hidden", "true");
+  };
+
+  emblaApi
+    .on("autoplay:timerset", startProgress)
+    .on("autoplay:timerstopped", stopProgress);
+
+  console.info("registered");
+
+  return () => {
+    console.info("unregistered");
 
     emblaApi
-      .on("reInit", setTweenNodes)
-      .on("reInit", setTweenFactor)
-      .on("reInit", tweenScale)
-      .on("scroll", tweenScale)
-      .on("slideFocus", tweenScale);
-
-    return () => {
-      tweenNodes.forEach((slide) => slide.removeAttribute("style"));
-    };
+      .off("autoplay:timerset", startProgress)
+      .off("autoplay:timerstopped", stopProgress);
   };
+};
 
+createScriptElement(
+  "https://unpkg.com/embla-carousel/embla-carousel.umd.js",
+  () => {
+    createScriptElement(
+      " https://unpkg.com/embla-carousel-autoplay/embla-carousel-autoplay.umd.js",
+      () => {
+        const emblaNode = document.querySelector(".team .embla");
+        const options = { loop: false, dragFree: true };
+        const prevBtn = document.querySelector("#embla-prev-team");
+        const nextBtn = document.querySelector("#embla-next-team");
+        const progressNode = document.querySelector("#embla-progress");
+        const emblaApi = EmblaCarousel(emblaNode, options, [
+          EmblaCarouselAutoplay({ playOnInit: 1, delay: 3000 }),
+        ]);
+        const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
+          emblaApi,
+          prevBtn,
+          nextBtn
+        );
+
+        const removeProgressListeners = addAutoplayProgressListeners(
+          emblaApi,
+          progressNode
+        );
+
+        document.querySelector(".team .embla").onmouseenter = () => {
+          emblaApi?.plugins().autoplay.stop();
+          console.info("enter");
+        };
+
+        document.querySelector(".team .embla").onmouseleave = () => {
+          emblaApi?.plugins().autoplay.play();
+          console.info("leave");
+        };
+
+        emblaApi
+          .on("destroy", removePrevNextBtnsClickHandlers)
+          .on("destroy", removeProgressListeners);
+
+        console.info("done");
+      }
+    );
+  }
+);
+
+if (window.innerWidth <= 1000) {
   createScriptElement(
     "https://unpkg.com/embla-carousel/embla-carousel.umd.js",
     () => {
-      const emblaNode = document.querySelector(".embla");
+      const emblaNode = document.querySelector(".usecases .embla");
       const options = { loop: false };
-      const opacityOptions = { active: 1, inactive: 0.3 }; // adjust as needed
+      const dots = document.querySelector(".usecases #embla-dots");
       const prevBtn = emblaNode.querySelector("#embla-prev");
       const nextBtn = emblaNode.querySelector("#embla-next");
-      const dotsNode = document.querySelector("#embla-dots");
-
       const emblaApi = EmblaCarousel(emblaNode, options, []);
-
       const removeTweenScale = setupTweenScale(emblaApi);
-
       const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
         emblaApi,
         prevBtn,
@@ -221,7 +319,7 @@ if (window.innerWidth <= 1000) {
       );
       const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(
         emblaApi,
-        dotsNode
+        dots
       );
 
       emblaApi
