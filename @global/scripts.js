6525316136eefd098bb85628f309e697c5ab7252
Arnaud <arnaud@status.im>
2025-08-11 19:08:34 +0200

    Try new footer

--- @global/scripts.js
+++ @global/scripts.js
@@ -103,108 +103,10 @@ function stickyMenu() {
   window.addEventListener("scroll", throttle(validateHeader, 100));
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-function footer() {
-  const observer = new IntersectionObserver(
-    (entries, obs) => {
-      entries.forEach(async (entry) => {
-        if (entry.isIntersecting) {
-          await waitScript("https://cdn.jsdelivr.net/npm/underscore@1.13.7");
+async function footer() {
 
-          (function (w, d) {
-            const options = ["ⓒ", "🅾", "Ɗ", "ė", "✖"],
-              hovers = ["hover", "hover2"],
-              map = {};
 
-            const setup = () => {
-              w.onresize = _.debounce(() => redraw(), 250);
 
-              d.onmousemove = (mev) => {
-                addAnim(mev.target, mev.buttons);
-              };
-              const getEl = (x, y) => {
-                if (map[x + "_" + y]) return map[x + "_" + y];
-                let e = d.elementFromPoint(x, y);
-                if (e) map[x + "_" + y] = e;
-                return e;
-              };
-              const touchHandler = (tev) => {
-                tev.preventDefault();
-                for (let i = 0; i < tev.changedTouches.length; i++) {
-                  let el = getEl(
-                    tev.changedTouches[i].pageX,
-                    tev.changedTouches[i].pageY
-                  );
-                  if (el) addAnim(el);
-                }
-              };
-              d.addEventListener(
-                "touchstart",
-                _.debounce(touchHandler, 250),
-                false
-              );
-              d.addEventListener("touchmove", touchHandler, false);
-              redraw();
-            };
-
-            const addAnim = (target, buttonPressed) => {
-              if (target.nodeName != "SPAN") return;
-              target.className = _.sample(hovers);
-              if (buttonPressed)
-                target.style.color = "#000000".replace(/0/g, function () {
-                  return (~~(Math.random() * 16)).toString(16);
-                });
-              target.innerHTML = _.sample(options);
-              _.delay(() => {
-                if (target.className.indexOf("hover") != -1) {
-                  target.className = "";
-                }
-              }, 750);
-            };
-
-            const redraw = () => {
-              const div = window.innerWidth < 800 ? 10 : 15;
-              let wh = d.clientHeight,
-                ww = window.outerWidth,
-                /*These arrays are to workaround CodePen's infinite loop "feature", large for-loops seem to trigger the error even if the loop isn't actually infinite :/ - Creating arrays padded with zeroes and using forEach seems to work (for now!!).*/
-                cols = new Array((ww / div) | 0).join("0").split(""),
-                rows = new Array((wh / div) | 0).join("0").split(""),
-                rh = [],
-                top = 5;
-              d.innerHTML = "";
-              rows.forEach(() => {
-                let spans = 0;
-                cols.forEach(() => spans++);
-                rh.push(spans);
-              });
-              rh.forEach((r) => {
-                if (r == rh[0]) {
-                  let row = '<div class="row">',
-                    left = 10;
-                  new Array(r)
-                    .join("0")
-                    .split("")
-                    .forEach(() => {
-                      row += `<span style="left:${left}px; top: ${top}px">+</span>`;
-                      left += 15;
-                    });
-                  row += "</div>";
-                  d.innerHTML += row;
-                  top += 15;
-                }
-              });
-            };
-
-            setup();
-          })(window, document.getElementById("background"));
-        }
-      });
-    },
-    {
-      threshold: 0.1, // Adjust as needed (0.5 = 50% visible)
-    }
-  );
-
-  observer.observe(document.querySelector(".blog"));
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 async function addSmoothScroll() {
@@ -318,7 +220,7 @@ function newsletter() {
 waitForPageLoaded().then(() => {
   addRandomText();
   stickyMenu();
-  // footer();
+  footer();
   addSmoothScroll();
   newsletter();
 });
